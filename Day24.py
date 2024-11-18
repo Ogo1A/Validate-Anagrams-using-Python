b4fe2583c78219c6a364a121d08cd5aea0e85e8f
Darren Ogola <darren.sunday@strathmore.edu>
2024-11-18 23:02:47 +0300

    My Day24 of 365 challange

---
+++ Day24.py
@@ -0,0 +1,7 @@
+def anagram(wordi, word2) :
+     wordi = word1. lower()
+     word2 = word2. lower)
+     return sorted(word1) == sorted(word2)
+print(anagram ("cinema", "iceman"))
+print (anagram("cool", "10co"))
+print (anagram ("men","women" ))
